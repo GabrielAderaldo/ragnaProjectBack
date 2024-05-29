2c5d6e376228e8b91b45cd69db3c866c8e8b07a6
Gabriel Aderaldo <gabriel_aderaldo@edu.unifor.br>
2024-05-29 12:58:45 -0300

    [00] - database use cases

---
+++ src/utils/error/error.ts
@@ -0,0 +1,9 @@
+export class DatabaseError extends Error{
+    code: number;
+    cause: string;
+    constructor(message: string, code: number,cause:string) {
+        super(message);
+        this.code = code
+        this.cause = cause
+    }
+}
